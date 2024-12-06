70c1035b0329b14cf4ee95bf93a312ddbdfc3e43
dtila <contact@dtila.ro>
2024-12-06 15:42:40 +0200

    Add multi target Microsoft.Extensions.Logging.Abstractions

---
+++ logging-multitargets/TestConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace TestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+
+    internal static partial class OutboxLoggers
+    {
+        [LoggerMessage(Level = LogLevel.Information, Message = "OutboxBackgroundService starting...")]
+        internal static partial void LogStarting(this ILogger logger);
+
+    }
+}
+
+
+
